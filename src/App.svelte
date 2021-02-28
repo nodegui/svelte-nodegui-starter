5797737b289ac39370900951e5ac671c6d242100
Jamie Birch work laptop <jbizzle5@gmail.com>
2021-02-28 14:16:17 +0000

    Update to 0.0.1-alpha.5 (which exposes types) and warn about gotchas.

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,21 @@
     import StepOne from "./components/StepOne.svelte";
     import StepTwo from "./components/StepTwo.svelte";
     import nodeguiIcon from "../assets/nodegui.jpg";
+    import type { NSVElement, RNWindow } from "@nodegui/svelte-nodegui";
 
     const winIcon = new QIcon(nodeguiIcon);
 
+    /**
+     * The exact type for this is: NSVElement<RNWindow>
+     * ... However, the Svelte language tools erroneously expect all bind:this values to extend HTMLElement, so
+     * for now, we have to rely on reasserting the type.
+     */
     let win;
 
     onMount(() => {
-        (window as any).win = win; // Prevent garbage collection.
-        win.nativeView.show();
+        (window as any).win = win; // Prevent garbage collection, otherwise the window quickly disappears!
+        (win as NSVElement<RNWindow>).nativeView.show();
+
         return () => {
             delete (window as any).win;
         };
@@ -36,6 +43,16 @@
 </window>
 
 <style>
+    /* 
+     * CSS has a few gotchas for now.
+     * 1) Some values need to be enclosed with quotes (e.g. `width: '100%';` rather than `width: 100%;`).
+     *    See: https://github.com/nodegui/svelte-nodegui/issues/4
+     * 2) Classes are not supported yet; they're a bit weird in Qt5.
+          See: https://github.com/nodegui/svelte-nodegui/issues/6
+     * 3) You can't write element-level rules like `text { color: 'red'; }`, unless they're global (not scoped).
+     *    For scoped rules, you have to refer to the underlying native element, e.g. `QLabel { color: 'red'; }`.
+     *    See: https://github.com/nodegui/svelte-nodegui/issues/7
+     */
     #welcome-text {
         font-size: 24px;
         padding-top: 20px;
