1d0f0e28c6c478f24bcd4dd0caf56fd5af3487b9
jonasgrunert <jonas@grunert-berlin.de>
2021-04-21 09:25:29 +0200

    Minor readability changes
    Forgot an s on line 7 and therefore lne 21.
    A friend pointed out that the "pipe" array is the default, but "inherit" automatically uses the parents process stdio.

--- livereload.js
+++ livereload.js
@@ -4,21 +4,19 @@ const { normalize } = require("path");
 
 // 'disconnect' is not needed as it will only be raised, when either the child process or this process calss disconnect explicitly.
 // We are abel to ensure, that this will never happen, as we do not need communication with the spawned subprocess
-const terminationSignal = ["close", "exit"];
+const terminationSignals = ["close", "exit"];
 
 function startSubprocess() {
   return new Promise((res, rej) => {
     const [command, ...args] = process.argv.slice(2);
     const proc = spawn(normalize(command), args, {
-      stdio: ["pipe", "pipe", "pipe"],
+      // use parents stdio
+      stdio: "inherit",
       shell:
         process.platform === "win32" ||
         /^(msys|cygwin)$/.test(process.env.OSTYPE),
     });
-    proc.stdout.pipe(process.stdout);
-    proc.stderr.pipe(process.stderr);
-    process.stdin.pipe(proc.stdin);
-    terminationSignal.forEach((event) => proc.on(event, res));
+    terminationSignals.forEach((event) => proc.on(event, res));
     proc.on("error", rej);
   });
 }
