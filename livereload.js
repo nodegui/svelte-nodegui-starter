b6f36887ae965f7b702dfc5b326c8726d0490ce8
jonasgrunert <jonas@grunert-berlin.de>
2021-04-21 09:14:12 +0200

    Adding required changes
    Except for the handling of the disconnect signal all changes were valid and prove to work on windows.

--- livereload.js
+++ livereload.js
@@ -2,11 +2,14 @@
 const { spawn } = require("child_process");
 const { normalize } = require("path");
 
-const termiateSignals = ["close", "exit", ""];
+// 'disconnect' is not needed as it will only be raised, when either the child process or this process calss disconnect explicitly.
+// We are abel to ensure, that this will never happen, as we do not need communication with the spawned subprocess
+const terminationSignal = ["close", "exit"];
 
 function startSubprocess() {
   return new Promise((res, rej) => {
-    const proc = spawn(process.argv.slice(2).map(normalize).join(" "), {
+    const [command, ...args] = process.argv.slice(2);
+    const proc = spawn(normalize(command), args, {
       stdio: ["pipe", "pipe", "pipe"],
       shell:
         process.platform === "win32" ||
@@ -15,7 +18,7 @@ function startSubprocess() {
     proc.stdout.pipe(process.stdout);
     proc.stderr.pipe(process.stderr);
     process.stdin.pipe(proc.stdin);
-    termiateSignals.forEach((e) => proc.on(e, res));
+    terminationSignal.forEach((event) => proc.on(event, res));
     proc.on("error", rej);
   });
 }
